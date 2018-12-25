dd37b114f8733ec9ac699ebebbf9cda87765e86b
Chandrashekhar <chandrashekhar.naik@ge.com>
2018-12-25 16:59:47 +0530

    refactor

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- server add and destroy buttons -->
     <div class="mt-5 mb-5 d-flex justify-content-center" id="server-action">
       <div class="mr-2 text-center pointer" @click="addServer">
         <div><i class="fas fa-plus-circle fa-2x"></i></div>
@@ -10,6 +11,7 @@
         <div class="mt-1">Destroy</div>
       </div>
     </div>
+    <!-- available apps list -->
     <div id="available-apps" class="mt-3">
       <p class="pl-3 mb-1">Available Apps</p>
       <ul class="list-group d-flex flex-column">
@@ -20,6 +22,7 @@
           :class="app.name"
         >
           <div class="flex">{{ app.name }}</div>
+          <!-- add and remove app buttons -->
           <div>
             <span @click="removeApp(app.name)" class="mr-2 pointer"
               ><i class="fas fa-minus-circle fa-lg"></i
@@ -43,23 +46,23 @@ export default {
         { name: "Hadoop" },
         { name: "Rails" },
         { name: "Chronos" },
-        { name: "Storm"},
-        { name: "Spark"}
+        { name: "Storm" },
+        { name: "Spark" }
       ]
     };
   },
   methods: {
-    removeApp(appName) {
-      EventBus.$emit("remove-app", appName);
-    },
-    addApp(appName) {
-      EventBus.$emit("add-app", appName);
-    },
     addServer() {
       EventBus.$emit("add-server");
     },
     destroyServer() {
       EventBus.$emit("destroy-server");
+    },
+    addApp(appname) {
+      EventBus.$emit("add-app", appname);
+    },
+    removeApp(appname) {
+      EventBus.$emit("remove-app", appname);
     }
   }
 };
@@ -70,6 +73,12 @@ export default {
 }
 
 .list-group-item {
+  padding: 15px 20px;
+  background-color: #141414;
+  border-bottom: 1px solid #4a4a4a;
+  margin: 0px;
+  border-left: 2px solid red;
+
   &.Hadoop {
     border-left: 4px solid #92039e;
 
@@ -109,10 +118,5 @@ export default {
       color: #00b048;
     }
   }
-  padding: 15px 20px;
-  background-color: #141414;
-  border-bottom: 1px solid #4a4a4a;
-  margin: 0px;
-  border-left: 2px solid red;
 }
 </style>
